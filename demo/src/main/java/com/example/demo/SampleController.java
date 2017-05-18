60b1d2e503fcd5a815790a8be923ce5f0ec99720
iyashiyas <shiyas009@gmail.com>
2017-05-18 14:58:12 +0300

    new

---
+++ demo/src/main/java/com/example/demo/SampleController.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SampleController {
+
+	@RequestMapping("/sample")
+	public String sampleIt() {
+		return "Hello! This Is Spring";
+	}
+	
+}
